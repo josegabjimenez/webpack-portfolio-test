175c71a342796d4c10983cca680e42ab03423526
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2022-01-21 20:54:23 -0500

    Babel added to webpack (Compiles ecmascript)

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,15 @@ module.exports = {
 	resolve: {
 		extensions: ['.js', '.jsx'],
 	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
 };
