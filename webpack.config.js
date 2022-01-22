71559453a9d3efda32c86c0f89c37982c250cdd7
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2022-01-22 00:40:21 -0500

    Html, css and preproccesors like sass now are compiled by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,45 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
+	// Where is the core of our application
 	entry: './src/index.js',
+	// The output of the bundle
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
 	},
+	// The extensions we are using
 	resolve: {
 		extensions: ['.js', '.jsx'],
 	},
+	// Here we set the loaders (what kind of files we are going to use)
 	module: {
 		rules: [
 			{
-				test: /\.js$/,
-				exclude: /node_modules/,
+				test: /\.m?js$/, // Regex to find all the files with a specified extension
+				exclude: /node_modules/, // Exclude node_modules
 				use: {
-					loader: 'babel-loader',
+					loader: 'babel-loader', // The loader we use
 				},
+			},
+			{
+				test: /\.css|.scss$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
 			},
 		],
 	},
+	// Here we put plugins that can allow us have more features
+	plugins: [
+		// Serve html
+		new HtmlWebpackPlugin({
+			inject: true,
+			template: './public/index.html',
+			filename: './index.html',
+		}),
+		// Compiles all the css files in our project into a single one file
+		new MiniCssExtractPlugin(),
+	],
 };
