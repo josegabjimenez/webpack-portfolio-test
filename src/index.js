e86865006fad353ea35f338c6d33499652aa9753
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2022-01-28 23:46:31 -0500

    :poop: Just testing gitmoji commits

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import Template from '@templates/Template.js';
 import '@styles/main.css';
 import '@styles/vars.scss';
 
+console.log('test');
+
 (async function App() {
 	const main = null || document.getElementById('main');
 	main.innerHTML = await Template();
